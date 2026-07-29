f4cd112d41b76f830a3f3adaafa2b4487859b9a9
Ana Claudia <anacnogueira@gmail.com>
2026-07-29 15:36:15 -0300

    Arruma paginação para pegar dados reais da API

--- src/components/pagination.tsx
+++ src/components/pagination.tsx
@@ -1,11 +1,23 @@
 import { CaretLeftIcon, CaretRightIcon } from "@phosphor-icons/react";
 import ButtonIcon from "./button-icon";
 import Skeleton from "./skeleton";
+import useRefunds from "../contexts/refunds/hooks/use-refunds";
 
 /* TO DO: Implementar Hooks */
 export function Pagination () {
-    const isLoadingRefunds = false;
-    const refunds = [];
+    const { refunds, filters, isLoadingRefunds } = useRefunds();
+
+    function handlePreviousNavigation() {
+        if (refunds?.meta.previousPageUrl) {
+            filters.setPage(String(Number(filters.page ?? 1) - 1))
+        }
+    }
+
+    function handleNextNavigation() {
+        if (refunds?.meta.nextPageUrl) {
+            filters.setPage(String(Number(filters.page ?? 1) + 1));
+        }
+    }
 
     if (!refunds) return null;
 
@@ -14,19 +26,23 @@ export function Pagination () {
             <ButtonIcon 
                 icon={CaretLeftIcon}
                 className="p-1"
-                disabled={false}
+                disabled={!refunds.meta.previousPageUrl}
+                onClick={handlePreviousNavigation}
             />
             <span className="text-sm text-gray-200">
                 { isLoadingRefunds 
                     ? <PaginationSkeleton />
-                    : "Página 1 de 10"
+                    : (
+                        `${refunds.meta.currentPage}/${refunds.meta.lastPage}`
+                    )
                 }
             </span>
 
             <ButtonIcon
                 icon={CaretRightIcon}
                 className="p-1"
-                disabled={false}
+                disabled={!refunds.meta.nextPageUrl}
+                onClick={handleNextNavigation}
             />
         </div>
     )
